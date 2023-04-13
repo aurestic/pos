6c6c4e935381b1925b78cfd0995fb667be48b656
Jose Zambudio <jose@aurestic.es>
2023-04-13 09:17:33 +0200

    [IMP] pos_order_return_traceability: black, isort, prettier

--- pos_order_return_traceability/__manifest__.py
+++ pos_order_return_traceability/__manifest__.py
@@ -4,20 +4,13 @@
 {
     "name": "POS frontend return traceability",
     "summary": "Adds full return traceability to POS frontend",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.0",
     "author": "Odoo Community Association (OCA), Solvos",
     "website": "https://github.com/OCA/pos",
     "license": "AGPL-3",
     "category": "Point Of Sale",
-    "depends": [
-        'pos_order_mgmt',
-        'pos_order_return',
-    ],
-    'data': [
-        "views/pos_templates.xml",
-    ],
-    "qweb": [
-        'static/src/xml/pos.xml',
-    ],
-    'installable': True,
+    "depends": ["pos_order_mgmt", "pos_order_return",],
+    "data": ["views/pos_templates.xml",],
+    "qweb": ["static/src/xml/pos.xml",],
+    "installable": True,
 }
--- pos_order_return_traceability/tests/test_module.py
+++ pos_order_return_traceability/tests/test_module.py
@@ -6,21 +6,20 @@ from odoo.tests.common import TransactionCase
 
 
 class TestModule(TransactionCase):
-
     def setUp(self):
         super(TestModule, self).setUp()
 
         # Get Registry
-        self.PosOrder = self.env['pos.order']
-        self.AccountPayment = self.env['account.payment']
+        self.PosOrder = self.env["pos.order"]
+        self.AccountPayment = self.env["account.payment"]
 
         # Get Object
-        self.pos_product = self.env.ref('point_of_sale.whiteboard_pen')
-        self.pricelist = self.env.ref('product.list0')
-        self.partner = self.env.ref('base.res_partner_12')
+        self.pos_product = self.env.ref("point_of_sale.whiteboard_pen")
+        self.pricelist = self.env.ref("product.list0")
+        self.partner = self.env.ref("base.res_partner_12")
 
         # Create a new pos config and open it
-        self.pos_config = self.env.ref('point_of_sale.pos_config_main').copy()
+        self.pos_config = self.env.ref("point_of_sale.pos_config_main").copy()
         self.pos_config.open_session_cb()
 
     def test_return_traceability(self):
@@ -29,54 +28,66 @@ class TestModule(TransactionCase):
 
         self.assertTrue(order.is_returnable)
         prepared_line = order._prepare_done_order_line_for_pos(order_line)
-        self.assertEqual(prepared_line['id'], order_line.id)
-        self.assertEqual(prepared_line['qty_returnable'], order_line.qty)
+        self.assertEqual(prepared_line["id"], order_line.id)
+        self.assertEqual(prepared_line["qty_returnable"], order_line.qty)
 
         res_refund = order.refund()
-        order_refund = self.PosOrder.browse(res_refund['res_id'])
+        order_refund = self.PosOrder.browse(res_refund["res_id"])
         self.assertFalse(order.is_returnable)
         self.assertFalse(order_refund.is_returnable)
         prepared_line = order._prepare_done_order_line_for_pos(order_line)
-        self.assertEqual(prepared_line['qty_returnable'], 0)
+        self.assertEqual(prepared_line["qty_returnable"], 0)
 
-        self.assertTrue('is_returnable' in
-                        self.PosOrder._prepare_fields_for_pos_list())
+        self.assertTrue("is_returnable" in self.PosOrder._prepare_fields_for_pos_list())
 
     def _create_order(self):
         order_data = {
-            'id': u'0006-001-0010',
-            'to_invoice': True,
-            'data': {
-                'pricelist_id': self.pricelist.id,
-                'user_id': 1,
-                'name': 'Order 0006-001-0010',
-                'partner_id': self.partner.id,
-                'amount_paid': 0.9,
-                'pos_session_id': self.pos_config.current_session_id.id,
-                'lines': [[0, 0, {
-                    'product_id': self.pos_product.id,
-                    'price_unit': 0.9,
-                    'qty': 1,
-                    'price_subtotal': 0.9,
-                    'price_subtotal_incl': 0.9,
-                }]],
-                'statement_ids': [[0, 0, {
-                    'journal_id': self.pos_config.journal_ids[0].id,
-                    'amount': 0.9,
-                    'name': fields.Datetime.now(),
-                    'account_id':
-                    self.env.user.partner_id.property_account_receivable_id.id,
-                    'statement_id':
-                    self.pos_config.current_session_id.statement_ids[0].id,
-                }]],
-                'creation_date': u'2018-09-27 15:51:03',
-                'amount_tax': 0,
-                'fiscal_position_id': False,
-                'uid': u'00001-001-0001',
-                'amount_return': 0,
-                'sequence_number': 1,
-                'amount_total': 0.9,
-            }}
+            "id": u"0006-001-0010",
+            "to_invoice": True,
+            "data": {
+                "pricelist_id": self.pricelist.id,
+                "user_id": 1,
+                "name": "Order 0006-001-0010",
+                "partner_id": self.partner.id,
+                "amount_paid": 0.9,
+                "pos_session_id": self.pos_config.current_session_id.id,
+                "lines": [
+                    [
+                        0,
+                        0,
+                        {
+                            "product_id": self.pos_product.id,
+                            "price_unit": 0.9,
+                            "qty": 1,
+                            "price_subtotal": 0.9,
+                            "price_subtotal_incl": 0.9,
+                        },
+                    ]
+                ],
+                "statement_ids": [
+                    [
+                        0,
+                        0,
+                        {
+                            "journal_id": self.pos_config.journal_ids[0].id,
+                            "amount": 0.9,
+                            "name": fields.Datetime.now(),
+                            "account_id": self.env.user.partner_id.property_account_receivable_id.id,
+                            "statement_id": self.pos_config.current_session_id.statement_ids[
+                                0
+                            ].id,
+                        },
+                    ]
+                ],
+                "creation_date": u"2018-09-27 15:51:03",
+                "amount_tax": 0,
+                "fiscal_position_id": False,
+                "uid": u"00001-001-0001",
+                "amount_return": 0,
+                "sequence_number": 1,
+                "amount_total": 0.9,
+            },
+        }
 
         result = self.PosOrder.create_from_ui([order_data])
         order = self.PosOrder.browse(result[0])
